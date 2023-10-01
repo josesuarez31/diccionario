7fc7994c310584310f5f2f23c7dae5b62cc8b5e4
Jose <josesuarezc53@gmail.com>
2023-10-01 18:30:01 -0400

    first commit

--- polls/urls.py
+++ polls/urls.py
@@ -3,14 +3,13 @@ from django.urls import path
 from . import views
 
 app_name = "polls"
-
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),  # Muestra todos
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),  # Muestra uno
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),  # Muestra el resultado despues de votar
     # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/vote/", views.vote, name="vote"), # Este lo usamos para guardar el voto
 ]
